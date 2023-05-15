9e8d729568bdda581387877f0f18e25dfb887ca2
0crispy <krispastas@gmail.com>
2023-05-15 21:39:52 +0300

    nu jo

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,64 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+use std::convert::Infallible;
 
-    for stream in listener.incoming(){
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
+use hyper::body::Buf;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, Method, StatusCode};
+use futures::{TryStreamExt as _, StreamExt, Stream};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct User {
+    name: String,
+    password: String,
 }
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let mut lines = buf_reader.lines();
-    let request_line = lines.next().unwrap().unwrap();
-    println!("{}",request_line);
-    let method = request_line.split(' ').collect::<Vec<_>>()[0];
-    let contents =  request_line.split(' ').collect::<Vec<_>>()[1];
-    let (status_line, filename) = 
-    if method == "GET" {
-        //println!("{}",contents);
-        ("HTTP/1.1 200 OK", "hello.html")
-    }
-    else if method == "POST"{
-        //println!("{}",request_line);
-        ("HTTP/1.1 200 OK", "hello.html")
-    }
-    else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+
+async fn hello(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from(std::fs::read_to_string("hello.html").unwrap());
+        },
+        (&Method::POST, "/login") => {    
+            let body = hyper::body::to_bytes(req.into_body()).await.unwrap().iter()
+                .cloned()
+                .collect::<Vec<u8>>();
+            let body = String::from_utf8(body).unwrap();
+            let body = body.split('&').collect::<Vec<&str>>();
+            let name = body[0].strip_prefix("name=").unwrap();
+            let password = body[1].strip_prefix("password=").unwrap();
+            println!("name {}, password {}",name,password);
+        }
+        (&Method::GET, "/register") => {
+            *response.body_mut() = Body::from(std::fs::read_to_string("register.html").unwrap());
+        },
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+            *response.body_mut() = Body::from(std::fs::read_to_string("404.html").unwrap());
+        },
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
+    Ok(response)
+}
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // For every connection, we must make a `Service` to handle all
+    // incoming HTTP requests on said connection.
+    let make_svc = make_service_fn(|_conn| {
+        // This is the `Service` that will handle the connection.
+        // `service_fn` is a helper to convert a function that
+        // returns a Response into a `Service`.
+        async { Ok::<_, Infallible>(service_fn(hello)) }
+    });
+
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
