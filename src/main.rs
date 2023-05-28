5a4e61cb42f549ee018ba871778fe71898b47860
0crispy <krispastas@gmail.com>
2023-05-28 21:32:24 +0300

    veikia bet veikia blogai

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,15 @@
-use std::collections::HashMap;
 use std::convert::Infallible;
-use std::fs::OpenOptions;
-use std::io::Write;
-
-use hyper::body::Buf;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server, Method, StatusCode};
-use futures::{TryStreamExt as _, StreamExt, Stream, SinkExt};
-use hyper_tungstenite::HyperWebsocket;
+use futures::{StreamExt, SinkExt, TryStreamExt};
+use hyper_tungstenite::{HyperWebsocket, WebSocketStream};
 use hyper_tungstenite::tungstenite::Message as ClientMessage;
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 
-#[derive(Deserialize,Serialize, Debug)]
-struct Users {
-    users:Vec<User>
-}
-impl Users{
-    fn get_user(&self,username:&str) -> Option<&User>{
-        self.users.iter().find(|x|x.username==username)
-    }
-    fn add_user(&mut self, username:&str,password:&str){
-        self.users.push(User {username:username.to_string(),password:password.to_string() });
-    }
-
-}
-#[derive(Deserialize,Serialize,Debug)]
-struct User{
-    username:String,
-    password:String,
-}
 #[derive(Deserialize,Serialize, Debug)]
 struct Rooms {
     rooms:Vec<Room>
-}
-impl Rooms{
-    fn get_room(&self,name:&str) -> Option<&Room>{
-        self.rooms.iter().find(|x|x.name==name)
-    }
 }
 #[derive(Deserialize,Serialize,Debug)]
 struct Room{
@@ -45,24 +18,95 @@ struct Room{
     id:String,
     messages:Vec<Message>,
 }
-#[derive(Deserialize,Serialize,Debug)]
+#[derive(Deserialize,Serialize,Debug, Clone)]
 struct Message{
     author:String,
     time:String,
     message:String,
+}
+impl Message{
+    fn new(author:&str,message:&str) -> Self{
+        Self{
+            author: author.to_string(),
+            time: chrono::Utc::now().to_string(),
+            message: message.to_string(),
+        }
+    }
+}
+fn load_res(path:&str) -> String{
+    std::fs::read_to_string("res/".to_string() + path).unwrap()
+}
+fn load_res_raw(path:&str) -> Vec<u8>{
+    std::fs::read("res/".to_string() + path).unwrap()
+}
+fn get_room(room_id:&str) -> Option<Room>{
+    match std::fs::read_to_string(format!("data/rooms/{}.json",room_id)){
+        Ok(ok) => {
+            Some(serde_json::from_str(&ok).unwrap())
+        },
+        Err(_) => {
+            None
+        },
+    }
+}
+fn save_room(room:Room){
+    std::fs::write(format!("data/rooms/{}.json",room.id),serde_json::to_string_pretty(&room).unwrap()).unwrap();
+}
+fn send_message(room_id:&str, author:&str, message:&str) -> Message{
+    let mut room = get_room(room_id).unwrap();
+    let msg = Message::new(author,message);
+    room.messages.push(msg.clone());
+    save_room(room);
+    msg
+}
+fn get_messages(start_index:usize, room_id:&str) -> Vec<Message> {
+    let room = get_room(room_id).unwrap();
+    let messages = room.messages.clone();
+    let len = (messages.len()-start_index).min(10);
+    messages[start_index..(start_index + len)].to_vec()
 }
 async fn serve_websocket(websocket: HyperWebsocket) -> Result<(), hyper_tungstenite::tungstenite::Error> {
     let mut websocket = websocket.await?;
     while let Some(message) = websocket.next().await {
         match message?{
             ClientMessage::Text(text) => {
-                println!("{}" ,text);
-                websocket.send(ClientMessage::Text("whats up".to_string())).await?;
+                let prefix = text.split(":").collect::<Vec<_>>()[0];
+                let msg = text.strip_prefix(&(prefix.to_string() + ":")).unwrap();
+                match prefix{
+                    "pls" => {
+                        println!("Shut up man");
+                        #[derive(Deserialize)]
+                        struct Pls{
+                            id:String,
+                            count:usize,
+                        }
+                        let pls = serde_json::from_str::<Pls>(msg).unwrap();
+                        //send messages
+                        let messages = get_messages(pls.count,&pls.id);
+                        for message in messages{
+                            println!("fuck {}", pls.id);
+                            let send_msg = ("new_message:".to_string() + &serde_json::to_string(&message).unwrap()).to_string();
+                            websocket.send(ClientMessage::Text(send_msg)).await?;
+                        }
+                    },
+                    "msg" => {
+                        #[derive(Deserialize)]
+                        struct BasicMessage{
+                            author:String,
+                            id:String,
+                            msg:String,
+                        }
+                        let basic_msg = serde_json::from_str::<BasicMessage>(msg).unwrap();
+                        let sent_msg = send_message(&basic_msg.id, &basic_msg.author, &basic_msg.msg);
+                        let send_msg = ("new_message:".to_string() + &serde_json::to_string(&sent_msg).unwrap()).to_string();
+                        websocket.send(ClientMessage::Text(send_msg)).await?;
+                    },
+                    _ => {}
+                }
             },
             _ =>{}
         }
     }
-
     Ok(())
 }
 async fn hello(mut req: Request<Body>) -> Result<Response<Body>, hyper_tungstenite::tungstenite::Error> {
@@ -71,6 +115,7 @@ async fn hello(mut req: Request<Body>) -> Result<Response<Body>, hyper_tungsteni
 
         // Spawn a task to handle the websocket connection.
         tokio::spawn(async move {
+
             if let Err(e) = serve_websocket(websocket).await {
                 eprintln!("Error in websocket connection: {}", e);
             }
@@ -84,147 +129,56 @@ async fn hello(mut req: Request<Body>) -> Result<Response<Body>, hyper_tungsteni
 
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => {
-            *response.body_mut() = Body::from(std::fs::read_to_string("hello.html").unwrap());
-        },        
-        (&Method::POST, "/login") => {    
-            let body = hyper::body::to_bytes(req.into_body()).await.unwrap().iter()
-                .cloned()
-                .collect::<Vec<u8>>();
-            let body = String::from_utf8(body).unwrap();
-            let body = body.split('&').collect::<Vec<&str>>();
-
-            let username = body[0].strip_prefix("name=").unwrap();
-            let password = body[1].strip_prefix("hashedPassword=").unwrap();
-            
-            let users:Users = serde_json::from_str(&std::fs::read_to_string("data/users.json").unwrap()).unwrap();
-            if let Some(user) = users.get_user(username){
-                if user.password == password{
-                    //nice
-                }
-                else{
-                    //incorrect password!
-                }
-            }
-            else{
-                //no such user!
-            }
-            
-        }
-        (&Method::GET, "/register") => {
-            *response.body_mut() = Body::from(std::fs::read_to_string("register.html").unwrap());
-        },
-        (&Method::POST, "/tryLogin") => {
-            let body = hyper::body::to_bytes(req.into_body()).await.unwrap().iter()
-                .cloned()
-                .collect::<Vec<u8>>();
-            let body = String::from_utf8(body).unwrap();
-            let user:User = serde_json::from_str(&body).unwrap();            
-            
-            let mut output_message = "ok";
-            let users:Users = serde_json::from_str(&std::fs::read_to_string("data/users.json").unwrap()).unwrap();
-            if let Some(found_user) = users.get_user(&user.username){
-                if found_user.password == user.password{
-                    //nice
-                }
-                else{
-                    output_message = "wrong_password";
-                }
-            }
-            else{
-                output_message = "no_user";
-            }
-            #[derive(Serialize)]
-            struct OutputMessage<'a>{
-                message:&'a str,
-            }
-            *response.body_mut() = Body::from(serde_json::to_string(&OutputMessage{message:output_message}).unwrap());
-        },
-        (&Method::POST, "/tryRegister") => {
-            let body = hyper::body::to_bytes(req.into_body()).await.unwrap().iter()
-                .cloned()
-                .collect::<Vec<u8>>();
-            let body = String::from_utf8(body).unwrap();
-            let user:User = serde_json::from_str(&body).unwrap();            
-            let mut users:Users = serde_json::from_str(&std::fs::read_to_string("data/users.json").unwrap()).unwrap();
-            
-            let mut output_message = "ok";
-            if users.get_user(&user.username).is_none(){
-                users.add_user(&user.username, &user.password);
-                std::fs::write("data/users.json",serde_json::to_string_pretty(&users).unwrap()).unwrap();
-            }
-            else{
-                output_message = "taken";
-            }
-            #[derive(Serialize)]
-            struct OutputMessage<'a>{
-                message:&'a str,
-            }
-            *response.body_mut() = Body::from(serde_json::to_string(&OutputMessage{message:output_message}).unwrap());
+            *response.body_mut() = Body::from(load_res("hello.html"));
         },
         (&Method::POST, "/getRooms") => {
-            let rooms:Rooms = serde_json::from_str(&std::fs::read_to_string("data/rooms.json").unwrap()).unwrap();
+            let rooms:Vec<Room> = std::fs::read_dir("data/rooms")
+                .unwrap()
+                .into_iter()
+                .map(|x|{
+                    let path = x.unwrap().path();
+                    let path_str = path.to_str().unwrap();
+                    let room_id = path_str.strip_prefix("data/rooms/").unwrap().strip_suffix(".json").unwrap();
+                    get_room(room_id).unwrap()
+                })
+                .collect();
             #[derive(Serialize)]
-            struct Room{
+            struct BasicRoom{
                 name:String,
                 info:String,
                 id:String,
             }
             #[derive(Serialize)]
             struct OutputMessage{
-                rooms:Vec<Room>,
+                rooms:Vec<BasicRoom>,
             }
-            let output_rooms = rooms.rooms.iter().map(|x|Room{name:x.name.clone(),info:x.info.clone(),id:x.id.clone()}).collect::<Vec<_>>();
+            let output_rooms = rooms.iter().map(|x|BasicRoom{name:x.name.clone(),info:x.info.clone(),id:x.id.clone()}).collect::<Vec<_>>();
             *response.body_mut() = Body::from(serde_json::to_string(&OutputMessage{
                 rooms:output_rooms
             }).unwrap());
 
         },
-        /*(&Method::POST, "/joinRoom") => {
-            let body = hyper::body::to_bytes(req.into_body()).await.unwrap().iter()
-                .cloned()
-                .collect::<Vec<u8>>();
-            let body = String::from_utf8(body).unwrap();
-            #[derive(Deserialize)]
-            struct Room{
-                name:String,password:String
-            }
-            let room:Room = serde_json::from_str(&body).unwrap();
-            let (room_name,room_password):(&str,&str) = (&room.name,&room.password);
-            let rooms:Rooms = serde_json::from_str(&std::fs::read_to_string("data/rooms.json").unwrap()).unwrap();
-            let mut output_message = "ok";
-            if let Some(room) = rooms.get_room(room_name){
-                if room.password == room_password{
-                    //good
-                }
-                else{
-                    //incorrect password
-                    output_message = "wrong_password";
-                }
-            }
-            else{
-                //no such room?? unexpected error.
-                output_message = "unexpected";
-            }
-            #[derive(Serialize)]
-            struct OutputMessage<'a>{
-                message:&'a str,
-            }
-            *response.body_mut() = Body::from(serde_json::to_string(&OutputMessage{message:output_message}).unwrap());
-        },*/
         (&Method::POST, "/chat") => {
-            *response.body_mut() = Body::from(std::fs::read_to_string("chat.html").unwrap());
+            *response.body_mut() = Body::from(load_res("chat.html"));
         },
         (&Method::GET, "/favicon.ico") => {
-            *response.body_mut() = Body::from(std::fs::read("favicon.ico").unwrap());
+            *response.body_mut() = Body::from(load_res_raw("favicon.ico"));
         },
         (&Method::GET, "/room") => {
             let uri = req.uri().to_string();
             let room_id = uri.strip_prefix("/room?id=").unwrap();
-            *response.body_mut() = Body::from(std::fs::read_to_string("room.html").unwrap());
+            if get_room(room_id).is_some(){
+                let mut body_str = load_res("room.html");
+                body_str = body_str.replace("ROOM_ID", room_id);
+                *response.body_mut() = Body::from(body_str);
+            }
+            else{
+                *response.body_mut() = Body::from(load_res("room_not_found.html"));
+            }
         },
         (_,_path) => {
             *response.status_mut() = StatusCode::NOT_FOUND;
-            *response.body_mut() = Body::from(std::fs::read_to_string("404.html").unwrap());
+            *response.body_mut() = Body::from(load_res("404.html"));
         },
     };
 
@@ -233,22 +187,23 @@ async fn hello(mut req: Request<Body>) -> Result<Response<Body>, hyper_tungsteni
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // For every connection, we must make a `Service` to handle all
-    // incoming HTTP requests on said connection.
-    let make_svc = make_service_fn(|_conn| {
-        // This is the `Service` that will handle the connection.
-        // `service_fn` is a helper to convert a function that
-        // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
-    });
 
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr:std::net::SocketAddr = ([127, 0, 0, 1], 3000).into();
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    println!("listening on http://{}", addr);
+    let mut http = hyper::server::conn::Http::new();
+    http.http1_only(true);
+    http.http1_keep_alive(true);
 
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Listening on http://{}", addr);
-
-    server.await?;
-
-    Ok(())
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let connection = http
+            .serve_connection(stream, hyper::service::service_fn(hello))
+            .with_upgrades();
+        tokio::spawn(async move {
+            if let Err(err) = connection.await {
+                println!("Error serving HTTP connection: {:?}", err);
+            }
+        });
+    }
 }
